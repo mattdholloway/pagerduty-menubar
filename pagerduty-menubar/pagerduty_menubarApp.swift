3b9952acde5afc4231974aed4edab554401b775c
Matt Holloway <mattdholloway@pm.me>
2026-05-18 13:50:39 +0100

    Initial Commit

---
+++ pagerduty-menubar/pagerduty_menubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  pagerduty_menubarApp.swift
+//  pagerduty-menubar
+//
+//  Created by Matthew Holloway on 18/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct pagerduty_menubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
